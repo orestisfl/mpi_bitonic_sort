eeee499881ecdeebea8a755564c7f601d18cf9d4
vasilish <vasilis4ch@gmail.com>
2014-12-22 23:58:42 +0200

    Fixed Merge,Makefile and Added Tests for merge

--- Ver1/bitonic_tests.c
+++ Ver1/bitonic_tests.c
@@ -6,7 +6,7 @@
 
 int iterativeHighMerge(int *merged , int *a ,int *b ,int N );
 int iterativeLowMerge(int *merged , int *a ,int *b ,int N );
-void merge(int *a, int *b, size_t N);
+
 int mergeTest();
 
 int main()
@@ -157,54 +157,4 @@ int iterativeLowMerge(int *merged , int *a ,int *b ,int N )
 
 
 
-void merge(int *a, int *b, size_t N)
-{
-  /* a: result array
-   *      * b: 2nd array for the merging
-   *           * i: index of array a
-   *                * j: index of array b */
-  int j = 0;
-  int behind = 0;
 
-  for(int i = 0; i < N; ++i) {
-    if (behind) {
-      /* the smaller elements of are on the behind */
-      if (b[j] <= a[N - 1]) {
-        /* b[j] is still smaller than behind elements */
-        if(N - behind > i) {
-          behind++;
-          a[N - behind] = a[i];
-        } else {
-          /* start replacing behind elements
-           *                      * because we've reached the back_fits (i) */
-          behind--;
-        }
-        a[i] = b[j++];
-      } else {
-        /* behind element smaller than b[j]. put it in a[i]*/
-        int repl = a[N - 1];
-        int back_fits = N - behind > i;
-
-        /* and swift the rest elements. */
-        memmove(&a[N - behind + 1], &a[N - behind], (behind - 1) * sizeof(int));
-        if(back_fits) a[N - behind] = a[i];
-        a[i] = repl;
-        if (!back_fits) behind--;
-      }
-
-    } else {
-      if (b[j] < a[i]) {
-        if(N - behind > i) {
-          behind++;
-          a[N - behind] = a[i];
-        } else {
-          /* start replacing behind elements
-           *                      * because we've reached the back_fits (i) */
-          behind--;
-        }
-        a[i] = b[j++];
-      }
-    }
-  }
-
-}
--- Ver1/merge.c
+++ Ver1/merge.c
@@ -1,27 +1,6 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//~ #include <math.h>
-//~ #include <time.h>
 
-#define SIZE 2000000
-void print_array(int *a, int N)
-{
-    for (int i = 0; i < N; ++i) printf("%d ", a[i]);
-    printf("\n");
-}
-
-int test(int *a)
-{
-    int pass = 1;
-    int i;
-    for (i = 1; i < SIZE; i++) {
-        pass &= (a[i - 1] <= a[i]);
-    }
-
-    printf("sorted TEST %s\n", (pass) ? "PASSed" : "FAILed");
-    return pass;
-}
 
 void merge(int *a, int *b, size_t N)
 {
@@ -30,102 +9,50 @@ void merge(int *a, int *b, size_t N)
      * i: index of array a
      * j: index of array b */
     int j = 0;
-    int beh = 0;
-    int cases[8] = {0};
+    int behind = 0;
 
     for(int i = 0; i < N; ++i) {
-        if (beh < 0) printf("WARNING beh<0\n");
-        if (beh) {
+        if (behind) {
             /* the smaller elements of are on the behind */
             if (b[j] <= a[N - 1]) {
-                cases[0] = 1;
-                /* b[j] is still smaller than beh elements */
-                if(N - beh > i) {
-                    cases[1] = 1;
-                    beh++;
-                    a[N - beh] = a[i];
+                /* b[j] is still smaller than behind elements */
+                if(N - behind > i) {
+                    behind++;
+                    a[N - behind] = a[i];
                 } else {
-                    cases[2] = 1;
-                    /* start replacing beh elements
+                    /* start replacing behind elements
                      * because we've reached the back_fits (i) */
-                    beh--;
+                    behind--;
                 }
                 a[i] = b[j++];
             } else {
-                cases[3] = 1;
-                /* beh element smaller than b[j]. put it in a[i]*/
+                /* behind element smaller than b[j]. put it in a[i]*/
                 int repl = a[N - 1];
-                int back_fits = N - beh > i;
+                int back_fits = N - behind > i;
 
-                if(back_fits) a[N - beh] = a[i];
-                if(back_fits) cases[4] = 1;
+                /* and switch the rest of the elements. */
+                memmove(&a[N - behind + 1], &a[N - behind] ,
+                    (behind - 1) * sizeof(int));
 
-                /* and swift the rest elements. */
-                memmove(&a[N - beh + 1], &a[N - beh], (beh - 1) * sizeof(int));
-                a[i] = repl;
-                if (!back_fits) beh--;
-                if (!back_fits) cases[5] = 1;
+                if(back_fits) a[N - behind] = a[i];
+                  a[i] = repl;
+                if (!back_fits) 
+                  behind--;
             }
 
         } else {
             if (b[j] < a[i]) {
-                cases[6] = 1;
-                if(N - beh > i) {
-                    cases[7] = 1;
-                    beh++;
-                    a[N - beh] = a[i];
+                if(N - behind > i) {
+                    behind++;
+                    a[N - behind] = a[i];
                 } else {
-                    cases[8] = 1;
-                    /* start replacing beh elements
+                    /* start replacing behind elements
                      * because we've reached the back_fits (i) */
-                    beh--;
+                    behind--;
                 }
                 a[i] = b[j++];
             }
         }
-        //~ printf("i=%d:\n", i);
-        //~ if (test(a)) printf(" ACK: ");
-        //~ else printf("NACK: ");
-        //~ print_array(a, SIZE);
-        
     }
 
-for (int l = 0; l < 8; ++l) {
-        if (!cases[l]) printf("!cases: %d\n", l);
-    }
-
-}
-
-void init(int *a, int *b)
-{
-    int i;
-    for (i = 0; i < SIZE; i++) {
-        a[i] = rand() % 100;
-        b[i] = rand() % 100;
-    }
-}
-
-
-int cmpfunc (const void * a, const void * b)
-{
-    return ( *(int*)a - * (int*)b );
-}
-
-
-int main(int argc , char* argv[] )
-{
-    
-    int a[SIZE], b[SIZE];
-    init(a, b);
-    qsort(a, SIZE, sizeof(int), cmpfunc);
-    printf("a:\n");
-    //~ print_array(a, SIZE);
-    qsort(b, SIZE, sizeof(int), cmpfunc);
-    printf("b:\n");
-    //~ print_array(b, SIZE);
-
-    merge(a, b, SIZE);
-    test(a);
-    //~ print_array(a, SIZE);
-    return 0;
 }

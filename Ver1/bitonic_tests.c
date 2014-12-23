ff8c2df4eca9a72155adf47a626f9e9182cb937e
vasilish <vasilis4ch@gmail.com>
2014-12-23 04:15:31 +0200

    Added Final and Correct Merge and Tests

--- Ver1/bitonic_tests.c
+++ Ver1/bitonic_tests.c
@@ -1,13 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define SIZE 10
+#define SIZE 50 
 
 
 int iterativeHighMerge(int *merged , int *a ,int *b ,int N );
 int iterativeLowMerge(int *merged , int *a ,int *b ,int N );
 
 int mergeTest();
+
+int compare( const void *a , const void *b)
+{
+  return ( (*(int*)a) - (*(int*)b) );
+}
 
 int main()
 {
@@ -20,6 +25,22 @@ int main()
 /* Performs a sequence of tests to see if the merge function
  * works as expected using simple iterative merge functions.
 */
+
+
+int comparisonTest( int * trueVal , int *valFound , int N )
+{
+  int i ;
+  int pass = 1 ;
+  for ( i = 0 ; i < N ; i++ )
+  {
+    pass &= ( trueVal[i] == valFound[i]  ) ; 
+    if ( trueVal[i] != valFound[i] )
+      printf(" Correct Value = %d Actual Value = %d \n",trueVal[i] ,
+          valFound[i] );
+  }
+  return pass;
+}
+
 
 int mergeTest()
 {
@@ -64,20 +85,14 @@ int mergeTest()
   printf("Finished Iterative Merge Low \n");
   merge( a , b , SIZE , 1 ) ;
   
-
-  int pass = 1 ;
-  for ( i = 0 ; i < SIZE ; i++ )
-  {
-    pass &= ( merged[i] == a[i]  ) ; 
-    if ( merged[i] != a[i] )
-      printf(" Correct Value = %d Actual Value = %d \n",merged[i] , a[i] );
-  }
-  
+  int pass = comparisonTest( merged , a , SIZE );
+ 
   printf("Merge Low test %s \n", pass ? "PASSed" : "FAILED");
 
 // ---------------------------------------------------------
 // ---------- Merge High Tests ----------------------------- 
-
+  
+  // b[i] - a[i] = 1 .
   printf("Generating Data Set for merge high test \n");
 
   for ( i = 0 ; i < SIZE ; i++ )
@@ -91,8 +106,60 @@ int mergeTest()
   printf("Finished iterative Merge \n");
   
   merge( a , b , SIZE , -1 ) ;
+ 
+  pass = comparisonTest( merged , a , SIZE ); 
+  printf("Merge High test 1 %s \n", pass ? "PASSed" : "FAILED");
+  
+  // b[i] - a[i] = SIZE .
 
+  for ( i = 0 ; i < SIZE ; i++ )
+  {
+    a[i] = i ;
+    b[i] = i + SIZE;
+  }
+  flag = iterativeHighMerge(merged , a , b , SIZE );
 
+  if ( flag < 0 ) 
+    return 0;
+
+  printf("Finished iterative Merge \n");
+  
+  merge( a , b , SIZE , -1 ) ;
+ 
+  pass = comparisonTest( merged , a , SIZE ); 
+  printf("Merge High test 2 %s \n", pass ? "PASSed" : "FAILED");
+  
+
+  // Random Numbers Arrays.
+
+  srand(time(NULL));
+  for ( i = 0 ; i < SIZE ; i++ )
+  {
+    a[i] = rand() % SIZE ;
+    b[i] = rand() % SIZE ;
+  }
+
+  // Sort them for the merge to work.
+  qsort( a , SIZE , sizeof(int) , compare );
+  qsort( b , SIZE , sizeof(int) , compare );
+
+  // Call the function that performs the correct merge.
+  flag = iterativeHighMerge(merged , a , b , SIZE );
+
+  if ( flag < 0 ) 
+    return 0;
+
+  printf("Finished iterative Merge \n");
+  
+  // Perform our merge.
+  merge( a , b , SIZE , -1 ) ;
+ 
+  pass = comparisonTest( merged , a , SIZE ); 
+  printf("Merge High Random Number test %s \n", pass ? "PASSed" : "FAILED");
+  
+  free(a);
+  free(b);
+  free(merged);
   return pass;
 }
 
@@ -108,32 +175,28 @@ int iterativeHighMerge(int *merged , int *a ,int *b ,int N )
     return 0;
   }
 
-  int i = 0 ;
-  int j = 0 ;
-  int k = 0 ;
+  int i = N - 1 ;
+  int j = N - 1 ;
+  int k = N - 1 ;
   
   // Merge the 2 arrays into one containing the smaller values of the 2 
   // arrays.
-  while ( k < N )
+  while ( k >= 0  )
   {
    
     if ( a[i] >= b[j] )
     {
       merged[k] = a[i];
-      i++;
-      k++;
+      i--;
+      k--;
     }
     else
     {
       merged[k] = b[j];
-      k++;
-      j++;
+      k--;
+      j--;
     }
   }
-
-  free(a);
-  free(b);
-  free(merged);
   // The merge was succesfull.
   return 1;
 }
